79b507a486e612cda3ef3d1a3786b019bf66b6bf
bzhangbridg <bingjie.zhang@bridg.com>
2016-05-29 20:32:34 -0700

    Finished Chapter 3: Adding favicon and Localized Time

--- hello.py
+++ hello.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap 
+from flask.ext.moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html', current_time = datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
